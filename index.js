f6f1b43a89ee580b51f88ff92bd8271fba49feb3
Noa'man Patel <normz@live.co.uk>
2020-04-05 20:07:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function dec(n){
   return n -= 1;
 }
 
-function makeInt(n){
+function increment(n){
   return parseInt(n, 10);
 }
 
-function preserveDecimal(n){
+function decrement(n){
   return parseFloat(n);
 }
